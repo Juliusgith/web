d305848110c7e1b561dce66ee3babbf16014dcd5
Juliusgith <rwothongeojulius052@gmail.com>
2024-08-17 13:41:41 +0300

    files

--- pss_project/settings.py
+++ pss_project/settings.py
@@ -141,11 +141,4 @@ MEDIA_ROOT = os.path.join(BASE_DIR, '/media/')
 # settings.py
 
 
-CELERY_BROKER_URL = 'redis://localhost:6379/0'
-CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
-CELERY_ACCEPT_CONTENT = ['json']
-CELERY_TASK_SERIALIZER = 'json'
-CELERY_RESULT_SERIALIZER = 'json'
-CELERY_TIMEZONE = 'UTC'
-
 
